33965b372b2baee01638d65f9534ce817ccbae32
mzaouchi <mzaouchi@gmail.com>
2023-12-04 16:50:52 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(){
+    super()
+    this.state={
+      db : [
+        {name : "Omar",isDone : false, id : Math.random()},
+        {name : "Abdelwaheb",isDone : false, id : Math.random()},
+        {name : "Omar",isDone : false, id : Math.random()}
+      ],
+      textP : ""
+    }
+  }
+
+  addData=()=> this.setState({db : [...this.state.db,{name : this.state.textP,isDone : false, id : Math.random()}],textP : ''})
+  deleteData=(delID)=> this.setState({db : this.state.db.filter((el,i,t)=>el.id !== delID ) }) 
+  doneData=(doneID)=> this.setState({db : this.state.db.map((el,i,t)=> el.id === doneID ? {...el,isDone : !el.isDone} : el)})
+  render(){
+    return(
+      <div>
+        <h1>Correction Chall State</h1>
+
+        {
+          this.state.db.map((el,i,t)=> 
+          <div>
+            <h2 className={el.isDone && 'under'}>{el.name}</h2>
+            <h3>{el.isDone ? 'TRUE' : 'FALSE'}</h3>
+            <button onClick={()=> this.doneData(el.id)}>Done</button>
+            <button onClick={()=>this.deleteData(el.id)}>Delete</button>
+          </div>
+          )
+        }
+        <br/>
+        <br/>
+        <input value={this.state.textP} type='text' onChange={(e)=> this.setState({textP : e.target.value}) }/>
+        <button onClick={this.addData}>Ajouter</button>
+      </div>
+    )
+  }
 }
 
 export default App;
